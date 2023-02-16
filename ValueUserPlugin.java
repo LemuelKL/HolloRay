d3c6ed2f10a0ee3c563a2b4e8c49caeeb1350a4c
LemuelKL <lemuellee.kl@gmail.com>
2023-02-16 22:51:54 +0000

    basic example

--- ValueUserPlugin.java
+++ ValueUserPlugin.java
@@ -12,7 +12,8 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
   static int SCREEN_WIDTH = 800;
   static int SCREEN_HEIGHT = 600;
 
-  Group root;
+  static Group root;
+  static Stage stage;
 
   @Override
   public String name() {
@@ -21,25 +22,37 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
 
   @Override
   public Value user(Value... args) throws ARTException {
-    root = new Group();
-    root.getChildren().add(new Label(args[0].toString() + "\n" + args[1].toString() + "\n" + args[2].toString()));
+    final Value arg1 = args[0];
+    final Value arg2 = args[1];
+    final Value arg3 = args[2];
 
-    System.out.println("I'm from Hong Kong!");
-    SpawnBox();
-
-    Stage stage = new Stage();
-    stage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
-    stage.setTitle("HolloRay");
-    stage.show();
-
-    return new __string("Hello, World!");
+    switch (args[0].value().toString()) {
+      case "init":
+        System.out.println("Initialising JavaFX stage");
+        stage = new Stage();
+        root = new Group();
+        return new __done();
+      case "paint":
+        System.out.println("Painting JavaFX stage");
+        stage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
+        stage.setTitle("HolloRay");
+        stage.show();
+        return new __done();
+      case "cube":
+        SpawnBox((double) arg2.value());
+        return new __done();
+      default:
+        System.out.println("Unknown internal command: " + args[0].value());
+        return new __string("OH NO!");
+    }
   }
 
-  private void SpawnBox() {
+  private void SpawnBox(double side_length) {
+    System.out.println("Spawning a box with side length " + side_length);
     Box box = new Box();
-    box.setDepth(50);
-    box.setHeight(50);
-    box.setWidth(50);
+    box.setDepth(side_length);
+    box.setHeight(side_length);
+    box.setWidth(side_length);
     root.getChildren().add(box);
   }
 }
