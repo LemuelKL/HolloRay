b7e70c81766a926965e7f8f776729f14ff419bfa
LemuelKL <lemuellee.kl@gmail.com>
2023-02-22 00:00:39 +0000

    fly cam via world shift

--- ValueUserPlugin.java
+++ ValueUserPlugin.java
@@ -6,6 +6,7 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Box;
@@ -33,11 +34,10 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
     static Group root;
     static Stage stage;
     static Scene scene;
-    static Translate pivot;
+    static Rotate sceneRotateX;
+    static Rotate sceneRotateY;
+    static Rotate sceneRotateZ;
     static Camera camera;
-    static Rotate cameraRotateX;
-    static Rotate cameraRotateY;
-    static Rotate cameraRotateZ;
 
     static int nextSolidId;
     static BiMap<Integer, javafx.scene.shape.Shape3D> solids;
@@ -63,7 +63,7 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
                 Shout("Clearing the scene");
                 root.getChildren().clear();
                 return new __done();
-            case "translate":
+            case "translate": {
                 int solidId = (int) args[1].value();
                 double x = (double) args[2].value();
                 double y = (double) args[3].value();
@@ -73,6 +73,14 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
                 solids.get(solidId).setTranslateY(solids.get(solidId).getTranslateY() + y);
                 solids.get(solidId).setTranslateZ(solids.get(solidId).getTranslateZ() + z);
                 return new __done();
+            }
+            case "rotate": {
+                int solidId = (int) args[1].value();
+                double angle = (double) args[2].value();
+                Shout("Rotating solid " + solidId + " by " + angle + " degrees");
+                solids.get(solidId).setRotate(solids.get(solidId).getRotate() + angle);
+                return new __done();
+            }
             case "cube":
                 spawnCube((double) args[1].value());
                 return new __int32(nextSolidId++, 0);
@@ -105,44 +113,56 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
         stage.setScene(scene);
         stage.setTitle("HolloRay");
         stage.show();
+        return new __done();
+    }
 
-        scene.setOnKeyPressed(event -> {
-            switch (event.getCode()) {
-                case Z:
-                    camera.setTranslateZ(camera.getTranslateZ() + 10);
-                    break;
-                case X:
-                    camera.setTranslateZ(camera.getTranslateZ() - 10);
-                    break;
-                case UP:
-                    camera.setTranslateY(camera.getTranslateY() - 10);
-                    break;
-                case DOWN:
-                    camera.setTranslateY(camera.getTranslateY() + 10);
-                    break;
-                case LEFT:
-                    camera.setTranslateX(camera.getTranslateX() - 10);
-                    break;
-                case RIGHT:
-                    camera.setTranslateX(camera.getTranslateX() + 10);
-                    break;
+    private Value initialise() {
+        Shout("Initialising JavaFX stage");
+        root = new Group();
+        scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, true);
+        stage = new Stage();
+
+        buildCamera();
+        buildAxes();
+
+        sceneRotateX = new Rotate(0, Rotate.X_AXIS);
+        sceneRotateY = new Rotate(0, Rotate.Y_AXIS);
+        sceneRotateZ = new Rotate(0, Rotate.Z_AXIS);
+        root.getTransforms().addAll(sceneRotateX, sceneRotateY, sceneRotateZ);
+
+        nextSolidId = 0;
+
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            switch (key.getCode()) {
                 case W:
-                    cameraRotateX.setAngle(cameraRotateX.getAngle() - 10);
+                    sceneRotateX.setAngle(sceneRotateX.getAngle() - 2);
                     break;
                 case S:
-                    cameraRotateX.setAngle(cameraRotateX.getAngle() + 10);
+                    sceneRotateX.setAngle(sceneRotateX.getAngle() + 2);
                     break;
                 case A:
-                    cameraRotateY.setAngle(cameraRotateY.getAngle() - 10);
+                    sceneRotateY.setAngle(sceneRotateY.getAngle() - 2);
                     break;
                 case D:
-                    cameraRotateY.setAngle(cameraRotateY.getAngle() + 10);
+                    sceneRotateY.setAngle(sceneRotateY.getAngle() + 2);
                     break;
                 case Q:
-                    cameraRotateZ.setAngle(cameraRotateZ.getAngle() - 10);
+                    sceneRotateZ.setAngle(sceneRotateZ.getAngle() - 2);
                     break;
                 case E:
-                    cameraRotateZ.setAngle(cameraRotateZ.getAngle() + 10);
+                    sceneRotateZ.setAngle(sceneRotateZ.getAngle() + 2);
+                    break;
+                case UP:
+                    root.setTranslateY(root.getTranslateY() + 10);
+                    break;
+                case DOWN:
+                    root.setTranslateY(root.getTranslateY() - 10);
+                    break;
+                case LEFT:
+                    root.setTranslateX(root.getTranslateX() + 10);
+                    break;
+                case RIGHT:
+                    root.setTranslateX(root.getTranslateX() - 10);
                     break;
                 default:
                     break;
@@ -152,36 +172,43 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
         return new __done();
     }
 
-    private Value initialise() {
-        Shout("Initialising JavaFX stage");
-        stage = new Stage();
-        root = new Group();
-        scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-        buildCamera();
-        buildAxes();
-
-        nextSolidId = 0;
-
-        return new __done();
-    }
-
     private void buildCamera() {
         camera = new PerspectiveCamera(true);
-        pivot = new Translate(0, 0, 0);
-        cameraRotateX = new Rotate(0, Rotate.X_AXIS);
-        cameraRotateY = new Rotate(0, Rotate.Y_AXIS);
-        cameraRotateZ = new Rotate(0, Rotate.Z_AXIS);
         camera.getTransforms().addAll(
-                pivot,
-                cameraRotateX,
-                cameraRotateY,
-                cameraRotateZ,
                 new Translate(0, 0, -1000));
 
-        camera.setNearClip(1);
-        camera.setFarClip(10000);
+        camera.setNearClip(0.1);
+        camera.setFarClip(100000);
         scene.setCamera(camera);
+    }
+
+    private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(240.0, 1, 1);
+        final Box yAxis = new Box(1, 240.0, 1);
+        final Box zAxis = new Box(1, 1, 240.0);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        final Group axisGroup = new Group(xAxis, yAxis, zAxis);
+        root.getChildren().addAll(axisGroup);
+
+        final Sphere origin = new Sphere(5);
+        origin.setMaterial(new PhongMaterial(Color.WHITE));
+        root.getChildren().add(origin);
     }
 
     private void spawnCube(double side_length) {
@@ -233,28 +260,4 @@ public class ValueUserPlugin implements ValueUserPluginInterface {
         // TODO
     }
 
-    private void buildAxes() {
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
-
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
-
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
-
-        final Box xAxis = new Box(240.0, 1, 1);
-        final Box yAxis = new Box(1, 240.0, 1);
-        final Box zAxis = new Box(1, 1, 240.0);
-
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
-
-        final Group axisGroup = new Group(xAxis, yAxis, zAxis);
-        root.getChildren().addAll(axisGroup);
-    }
 }
